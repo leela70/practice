b496d2d44f62cf5d691958b0b4ea0ea1d0eca2db
leela70 <139783211+leela70@users.noreply.github.com>
2023-09-10 14:44:35 +0530

    Add files via upload
    day 2

---
+++ myself/SmallestAndLargest.java
@@ -0,0 +1,26 @@
+package myself;
+
+public class SmallestAndLargest {
+
+	
+		 
+        public static void main(String[] args) {
+                int arr[] = {20, 10, 20, 30, 100};
+             
+                int largest = arr[0];
+                int smallest = arr[0];
+               
+                for(int i=1; i< arr.length; i++)
+                {
+                        if(arr[i] > largest)
+                                largest = arr[i];
+                        else if (arr[i] < smallest)
+                                smallest = arr[i];
+                       
+                }
+               
+                System.out.println(largest +" is largest");
+                System.out.println(smallest +" is smallest");                                                                                                                                                                                            
+        }
+}
+
